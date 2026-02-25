552a9807796405ae88fbd8d3a34fb1d433546cd4
Bhoomijarwani <bhoomijarwani2006@gmail.com>
2026-02-25 19:18:29 +0530

    Add files via upload

---
+++ Array/medium_level/pow_sum.cpp
@@ -0,0 +1,11 @@
+ #include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+ // problem link :https://leetcode.com/problems/powx-n/description/
+    int n = 10;
+    double x= 2.00000;
+     double result = pow(x,n);
+       cout <<  result <<endl;
+   return 0;     
+}
