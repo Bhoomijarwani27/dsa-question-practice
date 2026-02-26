ecaae5fd35e6f13ed8f393f477bcfac512c6c30b
Bhoomijarwani <bhoomijarwani2006@gmail.com>
2026-02-26 21:37:30 +0530

    Add files via upload

---
+++ Array/medium_level/maximum_subarray.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<climits>
+#include<vector>
+using namespace std; 
+//problem link : https://leetcode.com/problems/maximum-subarray/
+// Kadane's Algorithm it use only one loop 
+// most optimsed time cpmplexity 0(n)
+int main(){
+        vector<int>nums = {-2,1,-3,4,-1,2,1,-5,4};
+        int maxSum = INT_MIN;
+        int currSum = 0;
+      for(int i = 0; i < nums.size(); i++){
+            currSum += nums[i];
+            maxSum = max(currSum,maxSum);
+            if (currSum < 0){
+                currSum = 0;
+            }
+        }
+    
+     cout << maxSum;
+    }
